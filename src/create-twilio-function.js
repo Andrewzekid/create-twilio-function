83b1219b6bf7b8472eef20441e2f6d39910cc7cd
Andrewzekid <79450923+Andrewzekid@users.noreply.github.com>
2021-07-26 19:18:25 +0900

    TwilioQuest sample pull request(pls ignore)

--- src/create-twilio-function.js
+++ src/create-twilio-function.js
@@ -24,7 +24,7 @@ const successMessage = require('./create-twilio-function/success-message');
 
 async function cleanUpAndExit(projectDir, spinner, errorMessage) {
   spinner.fail(errorMessage);
-  spinner.start('Cleaning up project directories and files');
+  spinner.start('Cleaning up project directories and files lol');
   await rimraf(projectDir);
   spinner.stop().clear();
   process.exitCode = 1;
